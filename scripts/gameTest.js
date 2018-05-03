a11feb445591a31bc31579b44269c7b91e187ca5
Ingvar Óli <ingvaroli97@hotmail.com>
2018-05-03 22:24:59 +0000

    Fikt

--- scripts/gameTest.js
+++ scripts/gameTest.js
@@ -1,3 +1,4 @@
+var version = 1;
 var c = document.getElementById("testCanvas");
 var ctx = c.getContext("2d");
 var x = 50;
@@ -5,7 +6,7 @@ var x = 50;
 ctx.moveTo(0, 0);
 
 for (var i = 0; i <= 200; i++){
-    console.log(i);
+    console.log(i, version);
     ctx.lineTo(i, i);
 }
 ctx.stroke();
