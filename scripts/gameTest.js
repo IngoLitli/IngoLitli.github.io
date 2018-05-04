22a678884b3e547cf3312b68e9495d9779f1f424
Ingvar Óli <ingvaroli97@hotmail.com>
2018-05-04 18:04:12 +0000

    Fikt

--- scripts/gameTest.js
+++ scripts/gameTest.js
@@ -1,4 +1,4 @@
-var version = 4;
+var version = 5;
 var c = document.getElementById("testCanvas");
 var ctx = c.getContext("2d");
 var x = 50;
@@ -13,7 +13,7 @@ setTimeout(showTime(), 20);
 
 function showTime() {
     var d = new Date();
-    var seconds = d.getSeconds()
+    var seconds = d.getSeconds();
     ctx.lineTo(200, seconds*10);
     ctx.stroke();
     console.log(d.getSeconds());
