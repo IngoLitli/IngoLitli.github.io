6fff2aaba94ea885fbacfeecd9ce3baee11578d1
Ingvar Óli <ingvaroli97@hotmail.com>
2018-05-26 17:31:07 +0000

    Redirect á skolahysing

--- scripts/gameTest.js
+++ scripts/gameTest.js
@@ -1,1 +1,3 @@
-window.location.replace("http://www.skolahysing.com/nemendur/ingvaroli");
+//window.location.replace("http://www.skolahysing.com/nemendur/ingvaroli");
+
+console.Log(window.location);
